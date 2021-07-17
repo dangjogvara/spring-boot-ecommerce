1d416c58ed56f2bf28b67b7f3fdde8c088eb3e14
Dan Poulsen <dangjogvara@outlook.com>
2021-07-17 19:36:59 +0100

    implemented findByCategoryId

--- src/main/java/com/dangjogvara/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/dangjogvara/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.dangjogvara.ecommerce.dao;
 
-import com.dangjogvara.ecommerce.entity.Product;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.dangjogvara.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	// SELECT * FROM product WHERE category_id=?
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
