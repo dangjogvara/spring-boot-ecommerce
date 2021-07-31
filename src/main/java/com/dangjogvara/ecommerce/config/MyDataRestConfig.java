1d5eef184216dbcfb67734a8e03ae81f21445fbe
Dan Poulsen <dangjogvara@outlook.com>
2021-08-01 00:23:34 +0100

    expose entity ids

--- src/main/java/com/dangjogvara/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/dangjogvara/ecommerce/config/MyDataRestConfig.java
@@ -1,16 +1,32 @@
 package com.dangjogvara.ecommerce.config;
 
-import com.dangjogvara.ecommerce.entity.Product;
-import com.dangjogvara.ecommerce.entity.ProductCategory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import com.dangjogvara.ecommerce.entity.Product;
+import com.dangjogvara.ecommerce.entity.ProductCategory;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+	private EntityManager entityManager;
+
+	@Autowired
+	public MyDataRestConfig(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -30,6 +46,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 		// Configure cors mapping
 		cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
 
+		exposeIds(config);
+
+	}
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+		// expose entity id
+		// get a list of all entity classes from the entity manager
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+		// create an array of the entity types
+		List<Class> entityClasses = new ArrayList<>();
+
+		// get the entity types for the entities
+		for (EntityType tempEntityType : entities) {
+			entityClasses.add(tempEntityType.getJavaType());
+		}
+
+		// expose the entity ids for the entity/domain types
+		Class[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
 	}
 
 }
